909e8be9ce46589bf2369953c1d22c250e2667cb
Matt Gohr <gohr89@gmail.com>
2018-06-10 11:14:14 -0400

    first commit

---
+++ handler.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports.hello = (event, context, callback) => {
+const now = new Date();
+
+const message = `time is ${now}`
+
+console.log(message);
+
+callback(null, message);
+};
